83f53f37c17a97e777f1081d1bf2ac83f4104eb2
Alexandre Kimura <alexandremkimura@hotmail.com>
2021-08-18 22:12:50 -0300

    config GetStaticProps on about`s page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,21 @@
+import client from 'graphql/client';
+import { GET_PAGES } from 'graphql/queries';
 import AboutTemplate from 'templates/about';
 
-const About = () => <AboutTemplate />;
+export default function AboutPage() {
+  return <AboutTemplate />;
+}
 
-export default About;
+export const getStaticProps = async () => {
+  const { pages } = await client.request(GET_PAGES);
+
+  console.log(pages);
+
+  return {
+    props: {}
+  };
+};
+
+//getStaticPaths => Server para gerar as urls em build time => /about
+//getStaticProps => Buscar os dados da página(props) => build time
+//getServerSideProps => Buscar dados da página(props) => Runtime
